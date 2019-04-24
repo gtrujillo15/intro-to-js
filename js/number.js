848ad93e7e376704ebda5cf650e4215411476b2f
Gabriela T <gtrujillo15@gmail.com>
2019-04-23 19:04:07 -0700

    worked on number data types

---
+++ js/number.js
@@ -0,0 +1,37 @@
+// Number Data Type
+var a = 10;
+console.log(a);
+var b = 100;
+console.log(b);
+var a = 20;
+console.log(a);
+var c = 0x1E; //hex value of 30
+console.log(c);
+var d = 0.5;
+console.log(d);
+
+// addition
+var sum = a + b;
+console.log(sum);
+
+//subtraction
+var diff = d - c;
+console.log(diff);
+
+// multiplucation
+var times = a * d;
+console.log(times);
+
+// division
+var div = a / d;
+console.log(div);
+
+//modulus
+var mod = b % a;
+console.log(mod);
+var mod = b % c;
+console.log(mod);
+
+var e = '5';
+console.log(e);
+
