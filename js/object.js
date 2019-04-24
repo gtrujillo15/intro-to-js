be346d7057ccca19a40185589745c8e67bb7cf12
Gabriela T <gtrujillo15@gmail.com>
2019-04-23 20:56:41 -0700

    learned about object data types

---
+++ js/object.js
@@ -0,0 +1,27 @@
+// object data type
+var obj = {}; // empty object
+var car = {
+    make: 'honda', //make is a property of our object and honda is the value
+    model: 'civic',
+    year: '2019',
+};
+
+console.log(car);
+console.log(car.model);
+console.log(car.year);
+
+var date = {};
+// add date, month, year
+var date = {
+    day: 'Tuesday',
+    month: 4,
+    year: 2019,
+};
+
+//Display 'Tuesday' on console
+console.log(date.day);
+
+var userCred = {
+    username: 'gaby',
+    password: 'password',
+};
