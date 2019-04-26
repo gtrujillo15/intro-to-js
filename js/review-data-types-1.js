ec29ce75e7f858f3c3e0b38fc16d2df4ef611e22
Gabriela T <gtrujillo15@gmail.com>
2019-04-25 18:44:44 -0700

    review data types

---
+++ js/review-data-types-1.js
@@ -0,0 +1,42 @@
+var a = 1;
+var b = 2;
+var c = 100;
+var d = 300;
+var e = 6000;
+
+console.log('interger variables: ', a, b, c, d, e);
+
+var bool1 = true;
+var bool2 = false;
+console.log('boolean variables: ', bool1, bool2);
+
+var arr1 = [];
+var arr2 = [04, 25, 2019];
+var arr3 = ['Gabriela', 'Trujillo'];
+var arr4 = ['Thursday', 'April', 25];
+var arr5 = ['Yellow', 'Blue', null];
+console.log('array variables: ', arr1, arr2, arr3, arr4, arr5);
+
+var empty = {};
+var date = {
+    month: 4,
+    date: 25,
+    year: 2019
+};
+
+var user = {
+    firstName: 'Gabriela',
+    lastName: 'Trujillo'
+};
+
+var date2 = {
+    day: 'Thursday',
+    month: 'April',
+    day: 25
+};
+
+var colors = {
+    primary: 'red',
+    secondary: 'orange',
+}
+console.log('object variables: ', empty, date, user, date2, colors);
